4c79d024e583340d0e50a1ee9602d9f784902ea8
maheshthippala <mahesh.cubex@gmail.com>
2018-11-06 09:06:41 +0530

    Initial commit

---
+++ app/src/main/java/cubex/mahesh/rview_nov7am/MyHolder.kt
@@ -0,0 +1,23 @@
+package cubex.mahesh.rview_nov7am
+
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import kotlinx.android.synthetic.main.indiview.view.*
+
+class MyHolder(v:View) : RecyclerView.ViewHolder(v)
+{
+    var iview:ImageView? = null
+    var name:TextView? = null
+    var size:TextView? = null
+    var del:Button? = null
+   init { 
+       iview = v.iview
+       name = v.name
+       size = v.size
+       del = v.del
+   }
+
+}
